d96a1d945401761498cb518c3c90b6de1ea3e837
Christian Jánský <chrisjansky@gmail.com>
2017-06-21 11:31:08 +0200

    PUG SCSS JS JSON IMG First draft of homepage

---
+++ app/src/js/app.js
@@ -0,0 +1,12 @@
+var toggleObj = document.querySelectorAll('[data-toggle]');
+
+for (var i = 0; i < toggleObj.length; i++) {
+  toggleObj[i].onclick = function() {
+    var
+      toggleAttr = this.getAttribute('data-toggle'),
+      targetAttr = this.getAttribute('data-target'),
+      targetEl = targetAttr ? this.closest(targetAttr) : document.body;
+
+      targetEl.classList.toggle(toggleAttr + '--is-active');
+  }
+}
