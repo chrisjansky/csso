e1567b7a785d9960a6514ab9edd61925120e25cf
Christian Jánský <chrisjansky@gmail.com>
2017-07-05 17:42:20 +0200

    Installed smoothscroll-polyfill, added csso-scroll.js

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,8 +1,10 @@
 $.featherlightGallery.defaults.previousIcon = "←";
 $.featherlightGallery.defaults.nextIcon = "→";
 
+require('smoothscroll-polyfill').polyfill();
 require('./modules/csso-external')();
 require('./modules/csso-headroom')();
 require('./modules/csso-toggle')();
 require('./modules/csso-tabs')();
 require('./modules/csso-menu')();
+require('./modules/csso-scroll')();
---
+++ app/src/js/modules/csso-scroll.js
@@ -0,0 +1,14 @@
+module.exports = function() {
+  var links = document.querySelectorAll("[href^='#']");
+
+  for(var i = 0; i < links.length; i++) {
+    var link = links[i];
+
+    link.addEventListener("click", function() {
+      var target = this.getAttribute("href");
+
+      document.querySelector(target).scrollIntoView({ behavior: 'smooth' });
+    });
+
+  }
+}
