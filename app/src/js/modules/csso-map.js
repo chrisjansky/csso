34c1de3c057f3b72bd40a6637555e10a966e920f
Christian Jánský <chrisjansky@gmail.com>
2017-07-20 21:24:34 +0200

    JS Added !==null check to initMap()

--- app/src/js/modules/csso-map.js
+++ app/src/js/modules/csso-map.js
@@ -1,15 +1,19 @@
 module.exports = function() {
+  var mapEl = document.getElementById('js-map');
+
   function initMap() {
-    var address = {lat: 49.8254916, lng: 18.2491378};
-    var map = new google.maps.Map(document.getElementById('js-map'), {
-      zoom: 17,
-      center: address,
-      scrollwheel: false
-    });
-    var marker = new google.maps.Marker({
-      position: address,
-      map: map
-    });
+    if (mapEl !== null) {
+      var address = {lat: 49.8254916, lng: 18.2491378};
+      var map = new google.maps.Map(mapEl, {
+        zoom: 17,
+        center: address,
+        scrollwheel: false
+      });
+      var marker = new google.maps.Marker({
+        position: address,
+        map: map
+      });
+    }
   }
 
   initMap();  
